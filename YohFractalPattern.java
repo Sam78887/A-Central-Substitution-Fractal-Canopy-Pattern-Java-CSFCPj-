16e16d6299e50a374df8d8efaf2911f64a5d07a4
Sam78887 <117889420+Sam78887@users.noreply.github.com>
2026-03-27 14:59:11 +0530

    Update iteration prompt and remove limit but warning kept

--- YohFractalPattern.java
+++ YohFractalPattern.java
@@ -16,7 +16,7 @@ public class YohFractalPattern {
         // 1. INPUT STRING
         String seed = getUserInput(sc);
         
-        System.out.print("How many iterations? (Warning! : Entering more than 6 may result in System Crash!)");
+        System.out.print("How many iterations? (Warning! : Entering more than 6 may result in System Crash!) : ");
         int iterations = sc.nextInt();
 
         if (iterations < 1) return;
@@ -30,10 +30,7 @@ public class YohFractalPattern {
 
         if (iterations < 2) return;
 
-        if (iterations > 6) {
-            System.out.println("Limit reached. For system stability, iterations are capped at 6.");
-            iterations = 6;
-        }
+
 
         // PATTERN 2: Yoh + hoy = Yohhoy
         p[1] = p[0] + mirrorString(p[0]);
