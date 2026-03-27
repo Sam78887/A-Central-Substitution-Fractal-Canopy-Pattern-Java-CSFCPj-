9e756d05993fdd4f27f097bce1a32329d6e1ab1e
Sam78887 <117889420+Sam78887@users.noreply.github.com>
2026-03-27 15:00:58 +0530

    Update warning message for iterations input

--- YohFractalPattern.java
+++ YohFractalPattern.java
@@ -16,7 +16,7 @@ public class YohFractalPattern {
         // 1. INPUT STRING
         String seed = getUserInput(sc);
         
-        System.out.print("How many iterations? (Warning! : Entering more than 6 may result in System Crash!) : ");
+        System.out.print("How many iterations? (Warning! : Entering more than 15 may result in System Crash!) : ");
         int iterations = sc.nextInt();
 
         if (iterations < 1) return;
